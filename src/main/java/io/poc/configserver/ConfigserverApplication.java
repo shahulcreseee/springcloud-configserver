241e6f6c8ece015a53529bab8741e2e5cc970855
shahul <sbuhari@ntst.com>
2022-09-20 18:35:51 +0530

    Update: Eureka client and code cleanup.

--- src/main/java/io/poc/configserver/ConfigserverApplication.java
+++ src/main/java/io/poc/configserver/ConfigserverApplication.java
@@ -2,23 +2,16 @@ package io.poc.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
 		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
 )
-@RestController
 public class ConfigserverApplication {
-
-	@GetMapping("/healthcheck")
-	public String healthCheck(){
-		return "I am happy";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
